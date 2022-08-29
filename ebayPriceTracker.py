09b2e7ef1a265d3e9666f09fb835cbca6e66836e
hazzaRR <68497855+hazzaRR@users.noreply.github.com>
2022-08-29 16:26:43 +0100

    fully functioning webscrapper that gets all sold items information for a raspberry pi product. that stores the information in a pandas dataframe and then to a csv file

--- ebayPriceTracker.py
+++ ebayPriceTracker.py
@@ -12,9 +12,25 @@ def get_data(url):
     return soup
 
 def parse(soup):
-    results = soup.find_all('div', class_='s-item__info clearfix')
-    print(len(results))
-    return
+    productsList = []
+    products = soup.find_all('div', class_='s-item__info clearfix')
+
+    for i in range(1, len(products)):
+        product = {
+            'title': products[i].find('h3', class_='s-item__title s-item__title--has-tags').text,
+            'soldPrice': float(products[i].find('span', class_='s-item__price').text.replace('£', '').replace(',','')),
+            'soldDate' : products[i].find('div', class_='s-item__title--tagblock').find('span', class_='POSITIVE').text.replace('Sold  ', ''),
+            'bids': products[i].find('span', class_='s-item__bids s-item__bidCount').text,
+            'link': products[i].find('a', class_='s-item__link')['href']
+        }
+        productsList.append(product)
+    return productsList
+
+
+def output(products):
+    productsdf = pd.DataFrame(products)
+    productsdf.to_csv('raspberrypi4b.csv', index=False)
+    print(productsdf)
 
 soup = get_data(url)
-parse(soup)
+output(parse(soup))
